86e94d2e5868774aa41f97fac9d6aa873a20ffda
Rudaba-Kamor <rudabaadninkamor@gmail.com>
2023-03-07 20:03:15 +0600

    iterations

--- Module/Numpy/Iteration_np.py
+++ Module/Numpy/Iteration_np.py
@@ -4,3 +4,22 @@ myarr = np.array([11, 22, 33])
 
 for x in myarr:
   print(x)
+
+
+#####
+
+
+myarr = np.array([[1, 2, 3], [4, 5, 6]])
+
+''''for x in myarr:
+  print(x)'''
+for x in np.nditer(myarr):
+  print(x)
+
+ ##### 
+
+ar = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in ar:
+  for y in x:
+    print(y)
